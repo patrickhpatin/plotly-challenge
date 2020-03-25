757bdecd8f776e18ff6c2e07f1f5d30c38818765
Patrick Patin <40468092+patrickhpatin@users.noreply.github.com>
2020-03-25 11:27:53 -0400

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -47,15 +47,18 @@ var sampleData = d3.json("static/data/samples.json").then((incomingData) => {
     console.log("----------------------");
 
     names = sampleData.names; // ID
+    console.log(names);
     ethnicities = upack(sampleData.metadata, ETHNICITY);
-    genders = upack(sampleData.metadata, GENDER);
-    ages = upack(sampleData.metadata, AGE);
-    locations = upack(sampleData.metadata, LOCATION);
-    bbtypes = upack(sampleData.metadata, BBTYPE);
-    wfreqs = upack(sampleData.metadata, WFREQ);
+    console.log(ethnicities);
+
+    // genders = upack(sampleData.metadata, GENDER);
+    // ages = upack(sampleData.metadata, AGE);
+    // locations = upack(sampleData.metadata, LOCATION);
+    // bbtypes = upack(sampleData.metadata, BBTYPE);
+    // wfreqs = upack(sampleData.metadata, WFREQ);
 
     
-    for (var i = 0; i < ids.length; i++) {
+    for (var i = 0; i < names.length; i++) {
         // BIND DATA TO <select> ELEMENT.
         dropdownList.innerHTML = dropdownList.innerHTML +
             `<option value="${names[i]}">${names[i]}</option>`;
